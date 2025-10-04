8897347c8d1d9ca14923063ec1f04dbc3545903b
Miguel Santos Oliveira <miguel.santos321_@hotmail.com>
2025-10-03 23:20:44 -0300

    Jogo genius

---
+++ Jogo_Genius/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Jogo_Genius
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btVerde = new System.Windows.Forms.Button();
+            this.btAmarelo = new System.Windows.Forms.Button();
+            this.btVermelho = new System.Windows.Forms.Button();
+            this.btAzul = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbNivel = new System.Windows.Forms.Label();
+            this.btIniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btVerde
+            // 
+            this.btVerde.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btVerde.Location = new System.Drawing.Point(61, 49);
+            this.btVerde.Name = "btVerde";
+            this.btVerde.Size = new System.Drawing.Size(97, 97);
+            this.btVerde.TabIndex = 0;
+            this.btVerde.UseVisualStyleBackColor = true;
+            // 
+            // btAmarelo
+            // 
+            this.btAmarelo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btAmarelo.Location = new System.Drawing.Point(181, 49);
+            this.btAmarelo.Name = "btAmarelo";
+            this.btAmarelo.Size = new System.Drawing.Size(97, 97);
+            this.btAmarelo.TabIndex = 1;
+            this.btAmarelo.UseVisualStyleBackColor = true;
+            // 
+            // btVermelho
+            // 
+            this.btVermelho.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btVermelho.Location = new System.Drawing.Point(61, 162);
+            this.btVermelho.Name = "btVermelho";
+            this.btVermelho.Size = new System.Drawing.Size(97, 97);
+            this.btVermelho.TabIndex = 2;
+            this.btVermelho.UseVisualStyleBackColor = true;
+            // 
+            // btAzul
+            // 
+            this.btAzul.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btAzul.Location = new System.Drawing.Point(181, 162);
+            this.btAzul.Name = "btAzul";
+            this.btAzul.Size = new System.Drawing.Size(97, 97);
+            this.btAzul.TabIndex = 3;
+            this.btAzul.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nível:";
+            // 
+            // lbNivel
+            // 
+            this.lbNivel.AutoSize = true;
+            this.lbNivel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNivel.Location = new System.Drawing.Point(139, 282);
+            this.lbNivel.Name = "lbNivel";
+            this.lbNivel.Size = new System.Drawing.Size(0, 17);
+            this.lbNivel.TabIndex = 5;
+            // 
+            // btIniciar
+            // 
+            this.btIniciar.Location = new System.Drawing.Point(123, 341);
+            this.btIniciar.Name = "btIniciar";
+            this.btIniciar.Size = new System.Drawing.Size(97, 60);
+            this.btIniciar.TabIndex = 6;
+            this.btIniciar.Text = "Iniciar";
+            this.btIniciar.UseVisualStyleBackColor = true;
+            this.btIniciar.Click += new System.EventHandler(this.btIniciar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 450);
+            this.Controls.Add(this.btIniciar);
+            this.Controls.Add(this.lbNivel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btAzul);
+            this.Controls.Add(this.btVermelho);
+            this.Controls.Add(this.btAmarelo);
+            this.Controls.Add(this.btVerde);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Genius";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btVerde;
+        private System.Windows.Forms.Button btAmarelo;
+        private System.Windows.Forms.Button btVermelho;
+        private System.Windows.Forms.Button btAzul;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbNivel;
+        private System.Windows.Forms.Button btIniciar;
+    }
+}
+
---
+++ Jogo_Genius/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_Genius
+{
+    public partial class Form1 : Form
+    {
+        int sequenciaSelecionada = 1;
+        int sequenciaAtual = 0;
+        List<Button> Sequencia;
+        Color corPadrao;
+        Color corPiscando;
+        bool piscando = false;
+        int nivel = 0;
+
+        string corVerdePadrao = "#004700";
+        string corAmareloPadrao = "#F0B200";
+        string corVermelhoPadrao = "#A80A00";
+        string corAzulPadrao = "#06006B";
+
+        string corVerdePiscando = "#008A00";
+        string corAmareloPiscando = "#FFD047";
+        string corVermelhoPiscando = "#F50E00";
+        string corAzulPiscando = "#0C00D6";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            btVerde.BackColor = ColorTranslator.FromHtml(corVerdePadrao);
+            btAmarelo.BackColor = ColorTranslator.FromHtml(corAmareloPadrao);
+            btVermelho.BackColor = ColorTranslator.FromHtml(corVermelhoPadrao);
+            btAzul.BackColor = ColorTranslator.FromHtml(corAzulPadrao);
+
+            btVerde.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml(corVerdePiscando);
+            btAmarelo.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml (corAmareloPiscando);
+            btAzul.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml(corAzulPiscando);
+            btVermelho.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml(corVermelhoPiscando);
+           
+        }
+
+        private void btIniciar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
